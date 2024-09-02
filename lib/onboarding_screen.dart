410dff4500720662a4639e3db60f61e225d06b11
abmdeve <tundederome@gmail.com>
2024-09-03 01:47:05 +0200

    Init project
    Initialization the project and first commit

---
+++ lib/onboarding_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      ///appBar: AppBar(),
+      backgroundColor: const Color(0xff2C2B34),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/onboarding.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Premium cars. \nEnjoy the luxury',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    'Premium and prestige car daily rental \nExperience the thrill at a lower price',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: 320,
+                    height: 54,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white,
+                      ),
+                      child: const Text(
+                        'Let\'s Go',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/presentation/widgets/car_card.dart
@@ -0,0 +1,42 @@
+import 'package:car_rental_app_flutter_bloc/data/models/Car.dart';
+import 'package:flutter/material.dart';
+
+class CarCard extends StatelessWidget {
+  final Car car;
+
+  const CarCard({super.key, required this.car});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          Image.asset(
+            'assets/car_image.png',
+            height: 120,
+          ),
+          Text(
+            car.model,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          const SizedBox(height: 10,),
+          Row(
+            children: [
+              Row(
+                children: [
+                  Image.asset('assets/gps.png'),
+                  Text('${car.distance.toStringAsFixed(0)}L'),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
